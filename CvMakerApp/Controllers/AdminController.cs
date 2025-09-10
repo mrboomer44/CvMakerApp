03216e8bed57cfbf4fa448911babfe991d9afd20
mrboomer44 <yns.guldas44@gmail.com>
2025-09-10 17:19:14 +0300

    Admin Frontend tamamlandı !

---
+++ CvMakerApp/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CvMakerApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult Header()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Navbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Sidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Script()
+        {
+            return PartialView();
+        }
+    }
+}
--- CvMakerApp/Controllers/DefaultController.cs
+++ CvMakerApp/Controllers/DefaultController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CvMakerApp.Entity.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CvMakerApp.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly Context context;
+
+        public DefaultController(Context context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
